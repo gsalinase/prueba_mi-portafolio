f80c5edd1ec9fd2c85d4e20f7e223ba933574e49
Gonzalo Salinas <gsalinase91@gmail.com>
2020-01-08 20:53:59 -0300

    Add smooth scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,5 +3,19 @@ $(document).ready(function () {
     interval: 10000
   })
 
- 
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    };
+  });
 });
